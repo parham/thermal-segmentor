223526ede37933d14f00754fe6d21bceec4f21f9
Parham Nooralishahi <parham.nooralishahi@gmail.com>
2022-03-18 00:59:23 -0400

    update autoencoder implementation

--- phm/phm2022_autoencoder.py
+++ phm/phm2022_autoencoder.py
@@ -57,11 +57,11 @@ class Feature(nn.Module):
                  padding=1
                  ):
         super().__init__()
-        self.conv = nn.Conv2d(in_channel, out_channel,
-                              kernel_size=kernel_size,
-                              stride=stride,
-                              padding=padding
-                              )
+        self.conv = nn.Conv2d(
+            in_channel, out_channel,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding)
         self.bn = nn.BatchNorm2d(out_channel)
 
     def forward(self, x):
@@ -77,15 +77,13 @@ class TransposeFeature(nn.Module):
                  kernel_size=3,
                  stride=1,
                  padding=0,
-                 output_padding=0
-                 ):
+                 output_padding=0):
         super().__init__()
         self.conv = nn.ConvTranspose2d(in_channel, out_channel,
             kernel_size=kernel_size,
             stride=stride,
             padding=padding,
-            output_padding=output_padding
-        )
+            output_padding=output_padding)
         self.bn = nn.BatchNorm2d(out_channel)
 
     def forward(self, x):
@@ -95,91 +93,88 @@ class TransposeFeature(nn.Module):
         return x
 
 class phmAutoencoderModule (nn.Module):
-    def __init__(self, net_config, num_dim):
+    def __init__(self, config : DotMap, num_dim):
         super(phmAutoencoderModule, self).__init__()
         # Set the model's config based on provided configuration
-        self.config = net_config
-        self.nChannel = self.config['num_channels']
+        self.config = config
+        self.nChannel = self.config.num_channels
         # Part 01 : the feature extraction
         self.part01 = nn.ModuleList([
             Feature(num_dim, self.nChannel,
-                    kernel_size=self.config['part01_kernel_size'],
-                    stride=self.config['part01_stride'],
-                    padding=self.config['part01_padding']
-                    ),
+                kernel_size=self.config.part01_kernel_size,
+                stride=self.config.part01_stride,
+                padding=self.config.part01_padding),
             Feature(self.nChannel, self.nChannel,
-                    kernel_size=self.config['part01_kernel_size'],
-                    stride=self.config['part01_stride'],
-                    padding=self.config['part01_padding']
-                    ),
+                kernel_size=self.config.part01_kernel_size,
+                stride=self.config.part01_stride,
+                padding=self.config.part01_padding),
             Feature(self.nChannel, self.nChannel,
-                    kernel_size=self.config['part01_kernel_size'],
-                    stride=self.config['part01_stride'],
-                    padding=self.config['part01_padding']
-                    ),
+                kernel_size=self.config.part01_kernel_size,
+                stride=self.config.part01_stride,
+                padding=self.config.part01_padding),
         ])
         # Feature space including multiple convolutional layers
         # Part 02 : Auto-Encoder
         self.encoder = []
         inc_ch = self.nChannel
         # Encoder
-        for i in range(self.config['part02_num_layer']):
+        for i in range(self.config.part02_num_layer):
             tmp = Feature(inc_ch, inc_ch * 2,
-                          kernel_size=self.config['part02_kernel_size'],
-                          stride=self.config['part02_stride'],
-                          padding=self.config['part02_padding'])
+                kernel_size=self.config.part02_kernel_size,
+                stride=self.config.part02_stride,
+                padding=self.config.part02_padding)
             inc_ch *= 2
             self.encoder.append(tmp)
         self.encoder = nn.ModuleList(self.encoder)
         # Decoder
         self.decoder = []
-        for i in range(self.config['part02_num_layer']):
+        for i in range(self.config.part02_num_layer):
             tmp = TransposeFeature(int(inc_ch), int(inc_ch / 2),
-                                   kernel_size=self.config['part02_kernel_size'],
-                                   stride=self.config['part02_stride'],
-                                   padding=self.config['part02_padding'],
-                                   output_padding=self.config['part02_output_padding'])
+                                   kernel_size=self.config.part02_kernel_size,
+                                   stride=self.config.part02_stride,
+                                   padding=self.config.part02_padding,
+                                   output_padding=self.config.part02_output_padding)
             inc_ch /= 2
             self.decoder.append(tmp)
         self.decoder = nn.ModuleList(self.decoder)
         # Part 03 : the reference normalization for extracting class labels
         self.part03 = nn.ModuleList([
             Feature(self.nChannel, self.nChannel,
-                    kernel_size=self.config['part03_kernel_size'],
-                    stride=self.config['part03_stride'],
-                    padding=self.config['part03_padding']),
+                    kernel_size=self.config.part03_kernel_size,
+                    stride=self.config.part03_stride,
+                    padding=self.config.part03_padding),
             Feature(self.nChannel, self.nChannel,
-                    kernel_size=self.config['part03_kernel_size'],
-                    stride=self.config['part03_stride'],
-                    padding=self.config['part03_padding']),
+                    kernel_size=self.config.part03_kernel_size,
+                    stride=self.config.part03_stride,
+                    padding=self.config.part03_padding),
         ])
         self.part04 = nn.ModuleList([
             Feature(self.nChannel, self.nChannel,
-                                kernel_size=5,
-                                stride=1,
-                                padding=2),
+                kernel_size=5,
+                stride=1,
+                padding=2),
             Feature(self.nChannel, self.nChannel,
-                                kernel_size=5,
-                                stride=1,
-                                padding=2),
+                kernel_size=5,
+                stride=1,
+                padding=2),
             Feature(self.nChannel, self.nChannel,
-                                kernel_size=3,
-                                stride=1,
-                                padding=1),
+                kernel_size=3,
+                stride=1,
+                padding=1),
             Feature(self.nChannel, self.nChannel,
-                                kernel_size=5,
-                                stride=1,
-                                padding=2),
+                kernel_size=5,
+                stride=1,
+                padding=2),
             Feature(self.nChannel, self.nChannel,
-                                kernel_size=5,
-                                stride=1,
-                                padding=2),
+                kernel_size=5,
+                stride=1,
+                padding=2),
         ])
         # Part 04 : the final classification
         self.classify = Classifier(self.nChannel, self.nChannel,
-                                 kernel_size=self.config['part04_kernel_size'],
-                                 stride=self.config['part04_stride'],
-                                 padding=self.config['part04_padding'])
+            kernel_size=self.config.part04_kernel_size,
+            stride=self.config.part04_stride,
+            padding=self.config.part04_padding)
 
     def forward(self, x):
         # Part 01
