c231bd9f0879bf5198cac8979b31c364411b3021
Parham Nooralishahi <parham.nooralishahi@gmail.com>
2022-04-26 22:52:47 -0400

    implement fsim, ssim, issm, uiq, sam, sre metrics

--- phm/metrics.py
+++ phm/metrics.py
@@ -1,19 +1,16 @@
 
 
+import math
 import cv2
-import ignite
 import numpy as np
 import sklearn.metrics as skmetrics
+from skimage.metrics import structural_similarity
+import phasepack.phasecong as pc
 
 from typing import Any, Callable, Dict, List
-from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from comet_ml import Experiment
-from dotmap import DotMap
 
-from torchmetrics import Metric
-
-from ignite.metrics.metric import sync_all_reduce, reinit__is_reduced
 from ignite.exceptions import NotComputableError
 
 def segment_metric(name):
@@ -379,6 +376,8 @@ def rmse(org: np.ndarray, pred: np.ndarray, max_p: int = 255) :
 def psnr(org_img: np.ndarray, pred_img: np.ndarray, max_p: int = 255) -> float:
     """
     Peek Signal to Noise Ratio, implemented as mean squared error converted to dB.
+    Based on: https://github.com/up42/image-similarity-measures
+
     It can be calculated as
     PSNR = 20 * log10(MAXp) - 10 * log10(MSE)
     When using 12-bit imagery MaxP is 4095, for 8-bit imagery 255. For floating point imagery using values between
@@ -393,3 +392,248 @@ def psnr(org_img: np.ndarray, pred_img: np.ndarray, max_p: int = 255) -> float:
         mse_bands.append(np.mean(np.square(org[:, :, i] - pred[:, :, i])))
 
     return {'psnr' : 20 * np.log10(max_p) - 10.0 * np.log10(np.mean(mse_bands))}
+
+def _similarity_measure(x: np.array, y: np.array, constant: float):
+    """
+    Calculate feature similarity measurement between two images
+    """
+    numerator = 2 * x * y + constant
+    denominator = x ** 2 + y ** 2 + constant
+
+    return numerator / denominator
+
+def _gradient_magnitude(img: np.ndarray, img_depth: int):
+    """
+    Calculate gradient magnitude based on Scharr operator.
+    """
+    scharrx = cv2.Scharr(img, img_depth, 1, 0)
+    scharry = cv2.Scharr(img, img_depth, 0, 1)
+
+    return np.sqrt(scharrx ** 2 + scharry ** 2)
+
+def fsim(
+    org: np.ndarray, 
+    pred: np.ndarray, 
+    T1: float = 0.85, T2: float = 160
+) -> float:
+    """
+    Based on: https://github.com/up42/image-similarity-measures
+    Feature-based similarity index, based on phase congruency (PC) and image gradient magnitude (GM)
+    There are different ways to implement PC, the authors of the original FSIM paper use the method
+    defined by Kovesi (1999). The Python phasepack project fortunately provides an implementation
+    of the approach.
+    There are also alternatives to implement GM, the FSIM authors suggest to use the Scharr
+    operation which is implemented in OpenCV.
+    Note that FSIM is defined in the original papers for grayscale as well as for RGB images. Our use cases
+    are mostly multi-band images e.g. RGB + NIR. To accommodate for this fact, we compute FSIM for each individual
+    band and then take the average.
+    Note also that T1 and T2 are constants depending on the dynamic range of PC/GM values. In theory this parameters
+    would benefit from fine-tuning based on the used data, we use the values found in the original paper as defaults.
+    Args:
+        org_img -- numpy array containing the original image
+        pred_img -- predicted image
+        T1 -- constant based on the dynamic range of PC values
+        T2 -- constant based on the dynamic range of GM values
+    """
+    _assert_image_shapes_equal(org, pred, "FSIM")
+
+    org_img = org if len(org.shape) > 2 else org.reshape((org.shape[0], org.shape[1], 1))
+    pred_img = pred if len(pred.shape) > 2 else pred.reshape((pred.shape[0], pred.shape[1], 1))
+
+    alpha = (beta) = 1  # parameters used to adjust the relative importance of PC and GM features
+    fsim_list = []
+    for i in range(org_img.shape[2]):
+        # Calculate the PC for original and predicted images
+        pc1_2dim = pc(org_img[:, :, i], nscale=4, minWaveLength=6, mult=2, sigmaOnf=0.5978)
+        pc2_2dim = pc(pred_img[:, :, i], nscale=4, minWaveLength=6, mult=2, sigmaOnf=0.5978)
+
+        # pc1_2dim and pc2_2dim are tuples with the length 7, we only need the 4th element which is the PC.
+        # The PC itself is a list with the size of 6 (number of orientation). Therefore, we need to
+        # calculate the sum of all these 6 arrays.
+        pc1_2dim_sum = np.zeros((org_img.shape[0], org_img.shape[1]), dtype=np.float64)
+        pc2_2dim_sum = np.zeros((pred_img.shape[0], pred_img.shape[1]), dtype=np.float64)
+        for orientation in range(6):
+            pc1_2dim_sum += pc1_2dim[4][orientation]
+            pc2_2dim_sum += pc2_2dim[4][orientation]
+
+        # Calculate GM for original and predicted images based on Scharr operator
+        gm1 = _gradient_magnitude(org_img[:, :, i], cv2.CV_16U)
+        gm2 = _gradient_magnitude(pred_img[:, :, i], cv2.CV_16U)
+
+        # Calculate similarity measure for PC1 and PC2
+        S_pc = _similarity_measure(pc1_2dim_sum, pc2_2dim_sum, T1)
+        # Calculate similarity measure for GM1 and GM2
+        S_g = _similarity_measure(gm1, gm2, T2)
+
+        S_l = (S_pc ** alpha) * (S_g ** beta)
+
+        numerator = np.sum(S_l * np.maximum(pc1_2dim_sum, pc2_2dim_sum))
+        denominator = np.sum(np.maximum(pc1_2dim_sum, pc2_2dim_sum))
+        fsim_list.append(numerator / denominator)
+
+    return {'fsim' : np.mean(fsim_list)}
+
+def _ehs(x: np.ndarray, y: np.ndarray):
+    """
+    Entropy-Histogram Similarity measure
+    """
+    H = (np.histogram2d(x.flatten(), y.flatten()))[0]
+
+    return -np.sum(np.nan_to_num(H * np.log2(H)))
+
+def _edge_c(x: np.ndarray, y: np.ndarray):
+    """
+    Edge correlation coefficient based on Canny detector
+    """
+    # Use 100 and 200 as thresholds, no indication in the paper what was used
+    g = cv2.Canny((x * 0.0625).astype(np.uint8), 100, 200)
+    h = cv2.Canny((y * 0.0625).astype(np.uint8), 100, 200)
+
+    g0 = np.mean(g)
+    h0 = np.mean(h)
+
+    numerator = np.sum((g - g0) * (h - h0))
+    denominator = np.sqrt(np.sum(np.square(g - g0)) * np.sum(np.square(h - h0)))
+
+    return numerator / denominator
+
+def ssim(org_img: np.ndarray, pred_img: np.ndarray, max_p: int = 255) -> float:
+    """
+    Structural Simularity Index
+    Based on: https://github.com/up42/image-similarity-measures
+    """
+    _assert_image_shapes_equal(org_img, pred_img, "SSIM")
+
+    res = structural_similarity(org_img, pred_img, data_range=max_p, multichannel=True)
+    return {'ssim' : res}
+
+def issm(org: np.ndarray, pred: np.ndarray) -> float:
+    """
+    Information theoretic-based Statistic Similarity Measure
+    Note that the term e which is added to both the numerator as well as the denominator is not properly
+    introduced in the paper. We assume the authers refer to the Euler number.
+    Based on: https://github.com/up42/image-similarity-measures
+    """
+    _assert_image_shapes_equal(org, pred, "ISSM")
+
+    org_img = org if len(org.shape) > 2 else org.reshape((org.shape[0], org.shape[1], 1))
+    pred_img = pred if len(pred.shape) > 2 else pred.reshape((pred.shape[0], pred.shape[1], 1))
+
+    # Variable names closely follow original paper for better readability
+    x = org_img
+    y = pred_img
+    A = 0.3
+    B = 0.5
+    C = 0.7
+
+    ehs_val = _ehs(x, y)
+    canny_val = _edge_c(x, y)
+
+    numerator = canny_val * ehs_val * (A + B) + math.e
+    denominator = A * canny_val * ehs_val + B * ehs_val + C * ssim(x, y) + math.e
+
+    return {'issm' : np.nan_to_num(numerator / denominator)}
+
+def sliding_window(image: np.ndarray, stepSize: int, windowSize: int):
+    # slide a window across the image
+    for y in range(0, image.shape[0], stepSize):
+        for x in range(0, image.shape[1], stepSize):
+            # yield the current window
+            yield (x, y, image[y : y + windowSize[1], x : x + windowSize[0]])
+
+def uiq (org: np.ndarray, pred: np.ndarray, 
+    step_size: int = 1, 
+    window_size: int = 8
+):
+    """
+    Universal Image Quality index
+    Based on: https://github.com/up42/image-similarity-measures
+    """
+    # TODO: Apply optimization, right now it is very slow
+    _assert_image_shapes_equal(org, pred, "UIQ")
+
+    org = org.astype(np.float32)
+    pred = pred.astype(np.float32)
+
+    org_img = org if len(org.shape) > 2 else org.reshape((org.shape[0], org.shape[1], 1))
+    pred_img = pred if len(pred.shape) > 2 else pred.reshape((pred.shape[0], pred.shape[1], 1))
+
+    q_all = []
+    for (x, y, window_org), (x, y, window_pred) in zip(
+        sliding_window(org_img, stepSize=step_size, windowSize=(window_size, window_size)),
+        sliding_window(pred_img, stepSize=step_size, windowSize=(window_size, window_size)),
+    ):
+        # if the window does not meet our desired window size, ignore it
+        if window_org.shape[0] != window_size or window_org.shape[1] != window_size:
+            continue
+
+        for i in range(org_img.shape[2]):
+            org_band = window_org[:, :, i]
+            pred_band = window_pred[:, :, i]
+            org_band_mean = np.mean(org_band)
+            pred_band_mean = np.mean(pred_band)
+            org_band_variance = np.var(org_band)
+            pred_band_variance = np.var(pred_band)
+            org_pred_band_variance = np.mean(
+                (org_band - org_band_mean) * (pred_band - pred_band_mean)
+            )
+
+            numerator = 4 * org_pred_band_variance * org_band_mean * pred_band_mean
+            denominator = (org_band_variance + pred_band_variance) * (
+                org_band_mean ** 2 + pred_band_mean ** 2
+            )
+
+            if denominator != 0.0:
+                q = numerator / denominator
+                q_all.append(q)
+
+    if not np.any(q_all):
+        raise ValueError(
+            f"Window size ({window_size}) is too big for image with shape "
+            f"{org_img.shape[0:2]}, please use a smaller window size."
+        )
+
+    return {'uiq' : np.mean(q_all)}
+
+def sam(org: np.ndarray, pred: np.ndarray, convert_to_degree: bool = True):
+    """
+    Spectral Angle Mapper which defines the spectral similarity between two spectra
+    Based on: https://github.com/up42/image-similarity-measures
+    """
+    _assert_image_shapes_equal(org, pred, "SAM")
+
+    org_img = org if len(org.shape) > 2 else org.reshape((org.shape[0], org.shape[1], 1))
+    pred_img = pred if len(pred.shape) > 2 else pred.reshape((pred.shape[0], pred.shape[1], 1))
+
+    # Spectral angles are first computed for each pair of pixels
+    numerator = np.sum(np.multiply(pred_img, org_img), axis=2)
+    denominator = np.linalg.norm(org_img, axis=2) * np.linalg.norm(pred_img, axis=2)
+    val = np.clip(numerator / denominator, -1, 1)
+    sam_angles = np.arccos(val)
+    if convert_to_degree:
+        sam_angles = sam_angles * 180.0 / np.pi
+
+    # The original paper states that SAM values are expressed as radians, while e.g. Lanares
+    # et al. (2018) use degrees. We therefore made this configurable, with degree the default
+    return {'sam' : np.mean(np.nan_to_num(sam_angles))}
+
+def sre(org: np.ndarray, pred: np.ndarray):
+    """
+    Signal to Reconstruction Error Ratio
+    Based on: https://github.com/up42/image-similarity-measures
+    """
+    _assert_image_shapes_equal(org, pred, "SRE")
+
+    org_img = org if len(org.shape) > 2 else org.reshape((org.shape[0], org.shape[1], 1))
+    pred_img = pred if len(pred.shape) > 2 else pred.reshape((pred.shape[0], pred.shape[1], 1))
+
+    org_img = org_img.astype(np.float32)
+
+    sre_final = []
+    for i in range(org_img.shape[2]):
+        numerator = np.square(np.mean(org_img[:, :, i]))
+        denominator = (np.linalg.norm(org_img[:, :, i] - pred_img[:, :, i])) / (
+            org_img.shape[0] * org_img.shape[1])
+        sre_final.append(numerator / denominator)
+
+    return {'sre' : 10 * np.log10(np.mean(sre_final))}
